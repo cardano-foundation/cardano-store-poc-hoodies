b64764487348bca0a6780dd9c343c0d59726bf78
Michiel <michiel.bellen@gmail.com>
2023-09-22 13:55:13 +0200

    change cors

--- api.py
+++ api.py
@@ -119,8 +119,8 @@ def sdm_api_info():
 def _build_cors_preflight_response():
     response = make_response()
     response.headers.add("Access-Control-Allow-Origin", "*")
-    # response.headers.add('Access-Control-Allow-Headers', "*")
-    # response.headers.add('Access-Control-Allow-Methods', "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
     return response
 
 
